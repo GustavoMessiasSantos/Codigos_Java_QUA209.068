f1ad60b68d69c6758a16c64139e9278da3e884fa
GustavoMessiasSantos <gustavopessoal2023@gmail.com>
2023-08-02 17:50:51 -0300

    primeiro passo do exercicio

--- variaveisExercicio/src/variaveisExercicio/variaveisExercicio1.java
+++ variaveisExercicio/src/variaveisExercicio/variaveisExercicio1.java
@@ -3,6 +3,14 @@ package variaveisExercicio;
 public class variaveisExercicio1 {
 
 	public static void main(String[] args) {
+		int num1; int num2; char caractere1; char caracter2; String texto1; String texto2;
+ 		num1 = 100;
+		num2 = 200;
+		caractere1 = 'c';
+		caracter2 = 'p';
+		texto1 = "Gustavo";
+		texto2 = "Messias"; 
+		
 		
 
 	}
