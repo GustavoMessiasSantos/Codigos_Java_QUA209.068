33427bcbcd81dbc620adf7e2e181b2e61ea1021e
Gustavo Messias Santos <gustavopessoal2023@gmail.com>
2023-08-03 10:05:39 -0300

    Exercicios terminados

---
+++ variaveisExercicio/src/variaveisExercicio2/variaveisExercicio2.java
@@ -0,0 +1,17 @@
+package variaveisExercicio2;
+
+public class variaveisExercicio2 {
+
+	public static void main(String[] args) {
+		String nome;
+		String sobrenome;
+		
+		nome = "Gustavo";
+		sobrenome = " Messias Santos";
+		
+		System.out.println(nome + sobrenome);
+
+
+	}
+
+}
