17d67220a9e9a6c6842549941d953bea42c6383e
Gustavo Messias Santos <140188969+GustavoMessiasSantos@users.noreply.github.com>
2023-08-07 08:51:18 -0300

    exercicio de variáveis refeito

---
+++ variaveisExercicioRefeito/src/principal/variaveisExercicio2.java
@@ -0,0 +1,16 @@
+package principal;
+
+public class variaveisExercicio2 {
+
+	public static void main(String[] args) {
+		String nome;
+		String sobrenome;
+		
+		nome = "Gustavo";
+		sobrenome = " Messias Santos";
+		
+		System.out.println(nome + sobrenome);
+
+	}
+
+}
