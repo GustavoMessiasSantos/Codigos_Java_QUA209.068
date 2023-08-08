c0b003c991529fbc4adc50e6b8d01c44d89fd9a3
GustavoMessiasSantos <gustavopessoal2023@gmail.com>
2023-08-08 17:55:31 -0300

    Exercicios Estrutura de repetição

--- Exercicios_Estrutura_Condicional/src/principal/Media.java
+++ Exercicios_Estrutura_Condicional/src/principal/Media.java
@@ -6,6 +6,7 @@ public class Media {
 
 	public static void main(String[] args) {
 		
+		
 		Scanner leia = new Scanner(System.in);
 		
 		
@@ -15,24 +16,30 @@ public class Media {
 		double nota4;
 		double mediaFinal;
 		
+		System.out.println("Digite sua primeira nota: ");
 		nota1 = leia.nextDouble();
+		System.out.println("Digite sua segunda nota: ");
 		nota2 = leia.nextDouble();
+		System.out.println("Digite sua terceira nota: ");
 		nota3 = leia.nextDouble();
+		System.out.println("Digite sua quarta nota: ");
 		nota4 = leia.nextDouble();
 		
 		
 		mediaFinal = (nota1 + nota2 + nota3 + nota4)/4;
 		
-		if(mediaFinal <= 4) {
+		if(mediaFinal <= 6) {
 			System.out.println("Você está reprovado!");
-		}else if(mediaFinal >= 5 && mediaFinal < 7) {
+		}else if(mediaFinal >= 6 && mediaFinal < 7) {
 			System.out.println("Você está de recuperação!");
 		}else if(mediaFinal >= 7 && mediaFinal < 10) {
 			System.out.println("Você está aprovado!");
 		}else if(mediaFinal == 10) {
 			System.out.println("Você foi aprovado com honra!");
+		}else {
+			System.out.println("Você digitou algum número errado!");
 		}
-		
+		System.out.println("Sua média foi: " + mediaFinal);
 		
 		
 		leia.close();
